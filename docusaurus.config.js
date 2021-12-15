8b414ba54e4226f3f4db46af01ec2666b19890b1
Mex de Loo <mex@zeodev.cc>
2021-12-16 00:57:46 +0100

    fix: link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,8 +63,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Installation',
+                to: '/docs/installation/cosmo',
               },
             ],
           },
