f204c718fd6bb397a4bca01692cfa22792b41ada
Mex de Loo <mex@zeodev.cc>
2021-12-16 01:00:40 +0100

    update c onfig

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'TbdScripts',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  tagline: 'Morgan sucks',
+  url: 'https://docs.tbdscripts.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
